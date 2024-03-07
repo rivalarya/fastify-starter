f6826d8f9984593ad32b63aad69078f81ae78967
Rival Arya <rifall.aryap@gmail.com>
2024-03-07 11:12:54 +0700

    feat: add coerceTypes option

--- src/main.js
+++ src/main.js
@@ -3,6 +3,17 @@ const fastify = require('fastify')({
 })
 const config = require('../config.json')
 
+/**
+ * By default, coerceTypes: true. It means that if you create a request validation, for example: {id: { type: 'string' }} and the user passes the id as an integer, the framework will not return an error but will cast the integer to a string. But if you set coerceTypes:  false, it will return an error.
+ */
+// const Ajv = require('ajv')
+// const ajv = new Ajv({
+//   coerceTypes: false,
+// })
+// fastify.setValidatorCompiler(({ schema, method, url, httpPart }) => {
+//   return ajv.compile(schema)
+// })
+
 const pingDomain = require('./domains/ping/routes')
 
 fastify.register(pingDomain.routes, pingDomain.options)
