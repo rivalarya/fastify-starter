fa8a9455149b1bf56689a6794ab8d2d3d9639172
Rival Arya <rifall.aryap@gmail.com>
2024-09-01 19:34:51 +0700

    feat: update fastify version
    - move ajv initiation

--- src/main.js
+++ src/main.js
@@ -1,18 +1,14 @@
 const fastify = require('fastify')({
-  logger: true
+  logger: true,
+  ajv: {
+    // reference: https://fastify.dev/docs/latest/Reference/Server/#ajv
+    customOptions: {
+      removeAdditional: 'all',
+      coerceTypes: false
+    }
+  }
 })
 const config = require('../config.json')
-
-/**
- * By default, coerceTypes: true. It means that if you create a request validation, for example: {id: { type: 'string' }} and the user passes the id as an integer, the framework will not return an error but will cast the integer to a string. But if you set coerceTypes: false, it will return an error. https://fastify.dev/docs/latest/Reference/Validation-and-Serialization/#validator-compiler
- */
-const Ajv = require('ajv')
-const ajv = new Ajv({
-  coerceTypes: false
-})
-fastify.setValidatorCompiler(({ schema, method, url, httpPart }) => {
-  return ajv.compile(schema)
-})
 
 fastify.setErrorHandler(function (error, request, reply) {
   const statusCode = error.statusCode
