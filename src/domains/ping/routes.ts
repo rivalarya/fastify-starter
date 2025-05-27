a7d804aad06e95d27fa87a4060a0af836f033c8f
Rival Arya <rifall.aryap@gmail.com>
2025-05-27 15:49:46 +0700

    fix: update routeSchema example to ts

--- src/domains/ping/routes.ts
+++ src/domains/ping/routes.ts
@@ -1,9 +1,11 @@
 import { FastifyInstance, FastifyPluginAsync } from 'fastify'
 import { getPing } from './handler'
+import { getMessagesSchema } from './routesSchema'
 
 const ping: FastifyPluginAsync = async (fastify: FastifyInstance) => {
   fastify.get('/ping', {
     handler: getPing,
+    schema: getMessagesSchema
   })
 }
 
--- src/domains/ping/routesSchema.ts
+++ src/domains/ping/routesSchema.ts
@@ -1,29 +1,29 @@
 /**
- * Referrence: https://fastify.dev/docs/latest/Reference/Validation-and-Serialization/#validation
+ * Reference: https://fastify.dev/docs/latest/Reference/Validation-and-Serialization/#validation
  *
- * Usage example.
- * const getMessagesSchema = {
-  schema: {
-    querystring: {
-      type: 'object',
-      properties: {
-        fromId: { type: 'string' },
-        toId: { type: 'string' }
-      }
-    },
-    body: {
-      type: 'object',
-      required: ['fromId', 'toId', 'message'],
-      properties: {
-        fromId: { type: 'string' },
-        toId: { type: 'string' },
-        message: { type: 'string' }
-      }
-    }
-  }
-}
-
-module.exports = {
-  getMessagesSchema,
-}
+ * Usage example in TypeScript.
  */
+
+import { FastifySchema } from 'fastify';
+
+export const getMessagesSchema: FastifySchema = {
+  querystring: {
+    type: 'object',
+    properties: {
+      fromId: { type: 'string' },
+      toId: { type: 'string' }
+    },
+    required: [],
+    additionalProperties: false
+  },
+  // body: {
+  //   type: 'object',
+  //   required: ['fromId', 'toId', 'message'],
+  //   properties: {
+  //     fromId: { type: 'string' },
+  //     toId: { type: 'string' },
+  //     message: { type: 'string' }
+  //   },
+  //   additionalProperties: false
+  // }
+};
